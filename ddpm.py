1c8b36aefc199301a18728d44f2a475e81358e92
Sann-Htet <sannhtet899@gmail.com>
2023-11-10 03:32:39 +0630

    setting up for training codes

--- ddpm.py
+++ ddpm.py
@@ -1,9 +1,13 @@
 import os
+import copy
+import numpy as np
 import torch
 import torch.nn as nn
 import matplotlib.pyplot as plt
 from torch import optim
 from tqdm import tqdm
+from modules import EMA, UNet, UNet_conditional
+from utils import plot_images, save_images, get_data, setup_logging
 import logging
 from torch.utils.tensorboard import SummaryWriter
 
@@ -33,14 +37,17 @@ class Diffusion:
     def sample_timesteps(self, n):
         return torch.randint(low=1, high=self.noise_steps, size=(n,))
     
-    def sample(self, model, n):
+    def sample(self, model, n, labels, cfg_scale=3):
         logging.info(f"Sampling {n} new images...")
         model.eval()
         with torch.no_grad():
             x = torch.randn((n, 3, self.img_size, self.img_size)).to(self.device)
             for i in tqdm(reversed(range(1, self.noise_steps)), position=0):
                 t = (torch.ones(n) * i).long().to(self.device)
-                predicted_noise = model(x, t)
+                predicted_noise = model(x, t, labels)
+                if cfg_scale > 0:
+                    uncond_predicted_noise = model(x, labels, None)
+                    predicted_noise = torch.lerp(uncond_predicted_noise, predicted_noise, cfg_scale)
                 alpha = self.alpha[t][:, None, None, None]
                 alpha_hat = self.alpha_hat[t][:, None, None, None]
                 beta = self.beta[t][:, None, None, None]
@@ -53,3 +60,76 @@ class Diffusion:
         x = (x.clamp(-1, 1) + 1) / 2
         x = (x * 255).type(torch.uint8)
         return x
+    
+def train(args):
+    setup_logging(args.run_name)
+    device = args.device
+    dataloader = get_data(args)
+    model = UNet_conditional(num_classes=args.num_classes).to(device)
+    optimizer = optim.AdamW(model.parameters(), lr=args.lr)
+    mse = nn.MSELoss()
+    diffusion = Diffusion(img_size=args.image_size, device=device)
+    logger = SummaryWriter(os.path.join("runs", args.run_name))
+    l = len(dataloader)
+    ema = EMA(beta=0.995)
+    ema_model = copy.deepcopy(model).eval().requires_grad_(False)
+
+    for epoch in range(args.epochs):
+        logging.info(f"Starting epoch {epoch}:")
+        pbar = tqdm(dataloader)
+        for i, (images, labels) in enumerate(pbar):
+            images = images.to(device)
+            labels = labels.to(device)
+            t = diffusion.sample_timesteps(images.shape[0]).to(device)
+            x_t, noise = diffusion.noise_images(images, t)
+            if np.random.random() < 0.1:
+                labels = None
+            predicted_noise = model(x_t, t, labels)
+            loss = mse(noise, predicted_noise)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            ema.step_ema(ema_model, model)
+
+            pbar.set_postfix(MSE=loss.item())
+            logger.add_scalar("MSE", loss.item(), global_step=epoch * l + i)
+
+        if epoch % 10 == 0:
+            labels = torch.arange(10).long().to(device)
+            sampled_images = diffusion.sample(model, n=images.shape[0], labels=labels)
+            ema_sampled_images = diffusion.sample(ema_model, n=images.shape[0], labels=labels)
+            plot_images(sampled_images)
+            save_images(sampled_images, os.path.join("results", args.run_name, f"{epoch}.jpg"))
+            save_images(ema_sampled_images, os.path.join("results", args.run_name, f"{epoch}_ema.jpg"))
+            torch.save(model.state_dict(), os.path.join("models", args.run_name, f"ckpt.pt"))
+            torch.save(ema_model.state_dict(), os.path.join("models", args.run_name, f"ckpt_ema.pt"))
+            torch.save(optimizer.state_dict(), os.path.join("models", args.run_name, f"optim.pt"))
+
+def launch():
+    import argparse
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+    args.run_name = "DDPM_Uncondtional"
+    args.epochs = 50
+    args.batch_size = 12
+    args.image_size = 64
+    args.dataset_path = r"C:\Users\dome\datasets\landscape_img_folder"
+    args.device = "cpu"
+    args.lr = 3e-4
+    train(args)
+
+if __name__ == '__main__':
+    launch()
+    # device = "cuda"
+    # model = UNet().to(device)
+    # ckpt = torch.load("./working/orig/ckpt.pt")
+    # model.load_state_dict(ckpt)
+    # diffusion = Diffusion(img_size=64, device=device)
+    # x = diffusion.sample(model, 8)
+    # print(x.shape)
+    # plt.figure(figsize=(32, 32))
+    # plt.imshow(torch.cat([
+    #     torch.cat([i for i in x.cpu()], dim=-1),
+    # ], dim=-2).permute(1, 2, 0).cpu())
+    # plt.show()
